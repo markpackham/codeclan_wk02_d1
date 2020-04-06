d69c3eaaec89664c59a4785f4268d412d506c59b
mark <markpackham1@gmail.com>
2020-04-06 15:56:19 +0100

    Got book by title

--- library.rb
+++ library.rb
@@ -1,8 +1,15 @@
 class Library
-
   attr_reader :books
 
   def initialize(books)
     @books = books
+  end
+
+  def get_book(book)
+    for bo in books
+        if(bo[:title] == book)
+            return bo
+        end
+    end
   end
 end
--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -13,8 +13,12 @@ class TestLibrary < MiniTest::Test
 
   def test_get_books
     books = [{ title: "lord_of_the_rings", rental_details: { student_name: "Jeff", date: "01/12/16" } },
-        { title: "dune", rental_details: { student_name: "Anonymous", date: "06/06/06" } },
-        { title: "wuthering heights", rental_details: { student_name: "Liz", date: "16/04/19" } }]
+             { title: "dune", rental_details: { student_name: "Anonymous", date: "06/06/06" } },
+             { title: "wuthering heights", rental_details: { student_name: "Liz", date: "16/04/19" } }]
     assert_equal(books, @library.books)
+  end
+
+  def test_get_book
+    assert_equal({ title: "dune", rental_details: { student_name: "Anonymous", date: "06/06/06" } }, @library.get_book("dune"))
   end
 end
