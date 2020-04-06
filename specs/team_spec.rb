44ecbdf49d6266a4698ff2544a3aa30a46d1d12c
mark <markpackham1@gmail.com>
2020-04-06 14:35:11 +0100

    check for players

--- specs/team_spec.rb
+++ specs/team_spec.rb
@@ -6,6 +6,37 @@ require_relative("../team")
 
 class TestTeam < MiniTest::Test
     def setup()
-        @team = Team.new()
-      end
+        @team = Team.new("Newts",['bob','gary'],'steve')
+    end
+
+
+    def test_get_team_name
+      assert_equal("Newts", @team.team_name)
+    end
+  
+    def test_get_players
+      assert_equal(['bob','gary'], @team.players)
+    end
+
+    def test_get_coach
+      assert_equal('steve', @team.coach)
+    end
+    
+    def test_set_coach
+      @team.coach = 'brian'
+      assert_equal('brian', @team.coach)
+    end
+
+    def test_add_player
+      @team.add_player('mick')
+      assert_equal(['bob','gary','mick'], @team.players)
+    end
+
+    def test_check_for_player__not_in_array
+      assert_equal(false, @team.check_for_player('bruce'))
+    end
+
+    def test_check_for_player__in_array
+      assert_equal(true, @team.check_for_player('bob'))
+    end
 end
--- team.rb
+++ team.rb
@@ -1,3 +1,27 @@
 class Team
+    attr_reader :team_name, :players, :coach
+    attr_writer :coach
+  
+    def initialize(team_name, players, coach)
+      @team_name = team_name
+      @players = players
+      @coach = coach
+    end
+
+    def add_player(new_player)
+        @players.push(new_player)
+    end
+
+    def check_for_player(player)
+        for play in @players
+            if(play == player)
+                return true
+
+            else 
+                return false
+            end
+
+        end
+    end
 
 end
